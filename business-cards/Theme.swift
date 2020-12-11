e2dbba48cd18dc4576cd0cd4f20d6e9bee46c2ed
Douglas Webster <douglaswebster98@gmail.com>
2020-12-11 10:56:59 -0800

    initial commit

---
+++ business-cards/Theme.swift
@@ -0,0 +1,61 @@
+//
+//  Theme.swift
+//  business-cards
+//
+//  Created by Douglas Webster on 12/11/20.
+//
+
+import Foundation
+import UIKit
+
+public class Theme {
+    
+    public static var keyColor: UIColor = {
+        let standardKeyColor = UIColor(red: 241/256, green: 81/256, blue: 86/256, alpha: 1)
+        if #available(iOS 13, *) {
+            return UIColor { (traitCollection) -> UIColor in
+                switch traitCollection.userInterfaceStyle {
+                case .dark:
+                    return UIColor(red: 241/256,
+                                   green: 81/256,
+                                   blue: 86/256,
+                                   alpha: 1.0)
+                case .light: fallthrough
+                case .unspecified: fallthrough
+                @unknown default:
+                return standardKeyColor
+                }
+            }
+        } else {
+            return standardKeyColor
+        }
+    }()
+    
+    public static func getBoldLabel(text: String) -> UILabel {
+        let label = UILabel()
+        if #available(iOS 13, *) {
+            label.textColor = UIColor.label
+        }
+        label.text = text
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        return label
+    }
+    
+    public static func getButton(title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.setTitle(title, for: .normal)
+        
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        button.layer.cornerRadius = 4
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
+        button.contentEdgeInsets = UIEdgeInsets(top: 4, left: 10, bottom: 4, right: 10)
+        button.clipsToBounds = true
+        return button
+    }
+    
+}
---
+++ business-cards/ViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController.swift
+//  business-cards
+//
+//  Created by Douglas Webster on 12/10/20.
+//
+
+import Foundation
